21c14077aaeef87fbaf350cc458f68f6db965878
Wouter Geraedts <w.geraedts@onii.org>
2012-09-16 20:51:24 +0200

    Added extracting coursenames from filenames

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,38 @@
 
 #include <boost/filesystem.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/regex.hpp>
 
 #include "csv_parser.hpp"
+
+struct evaluation
+{
+	std::string code, name, id, time;
+	
+	evaluation(
+		const std::string& code,
+		const std::string& name,
+		const std::string& id,
+		const std::string& time
+	)
+	: code(code)
+	, name(name)
+	, id(id)
+	, time(time)
+	{}
+	
+	static evaluation parse(const std::string& str)
+	{
+		//Resultaten_Vragenlijst_44773_Functioneel Programmeren_IBC006 (2012-01-23)_xls.xls.csv
+		const static boost::regex r("Resultaten_Vragenlijst_([0-9]+)_(.+)_([^\\_]+) \\((.+)\\)_xls\\.xls\\.csv");
+		
+		boost::smatch match;
+		if(!boost::regex_match(str, match, r))
+			throw std::runtime_error("Regex failed");
+		
+		return evaluation(match[3], match[2], match[1], match[4]);
+	}
+};
 
 bool check_rating_row(const std::vector<std::string>& line, size_t& rating)
 {
@@ -49,8 +79,7 @@ int main(int argc, char** argv)
 	const directory_iterator it_end;
 	for(directory_iterator it(p); it != it_end; ++it)
 	{
-		const std::string filename(it->path().string());
-		csv_parser parser(filename);
+		csv_parser parser(it->path().string());
 		
 		std::map<double, double> rating_map;
 		std::vector<std::string> line;
@@ -77,8 +106,10 @@ int main(int argc, char** argv)
 		
 		stddev = std::sqrt(stddev / count);
 		
+		evaluation e = evaluation::parse(it->path().filename().string());
+		
 		std::cout
-			<< filename << '\n'
+			<< e.code << " - " << e.name << '\n'
 			<< "\tcount: " << count
 			<< " avg: " << round(avg, 1)
 			<< " stddev: " << round(stddev, 2)
