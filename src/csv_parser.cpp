a044336f173c3b5f2c75d42fad0d48958b77c6e1
Wouter Geraedts <w.geraedts@onii.org>
2012-09-16 12:18:15 +0200

    Improved compilation speed by moving #include from .hpp to .cpp

--- src/csv_parser.cpp
+++ src/csv_parser.cpp
@@ -1,6 +1,7 @@
 #include "csv_parser.hpp"
 
 #include <stdexcept>
+#include <boost/tokenizer.hpp>
 
 namespace vakkenranking
 {
@@ -18,7 +19,7 @@ namespace vakkenranking
 		if(!std::getline(s, line))
 			return false;
 		
-		tokenizer tok(line);
+		boost::tokenizer<boost::escaped_list_separator<char>> tok(line);
 		result.assign(tok.begin(), tok.end());
 		
 		return true;
