c7977d36ac3aac863e16adac2a549f6851403971
Wouter Geraedts <git@woutergeraedts.nl>
2014-07-17 13:48:52 +0200

    Added commandline interface

---
+++ src/cli.hpp
@@ -0,0 +1,160 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <map>
+#include <functional>
+
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/regex.hpp>
+#include <boost/optional.hpp>
+#include <boost/program_options.hpp>
+
+#include "csv_parser.hpp"
+#include "summary_parser.hpp"
+#include "complete_parser.hpp"
+
+#include "printer.hpp"
+
+namespace vakkenranking
+{
+
+class cli
+{
+	cli() = delete;
+	cli(cli&) = delete;
+	cli& operator=(cli&) = delete;
+
+	enum class output_e
+	{
+		HTML,
+		CSV
+	};
+
+	struct options
+	{
+		output_e output = output_e::HTML;
+		std::string datadir = "./data";
+	};
+
+	static std::vector<vakkenranking::evaluation> parse_data(const std::string& str, std::function<vakkenranking::evaluation(const boost::filesystem::path&)> parse_f)
+	{
+		using namespace vakkenranking;
+		using namespace boost::filesystem;
+
+		std::vector<evaluation> result;
+
+		path p(str);
+
+		const directory_iterator it_end;
+		for(directory_iterator it(p); it != it_end; ++it)
+			result.emplace_back(parse_f(it->path()));
+
+		return result;
+	}
+
+	static int interpret(options& opt, int argc, char** argv)
+	{
+		std::string outputstr = "html";
+
+		boost::program_options::options_description o_general("General options");
+		o_general.add_options()
+		("help,h", "display this message")
+		("outputtype,t", boost::program_options::value(&outputstr), "{html, csv} (defaults to html)")
+		("datadir,d", boost::program_options::value<decltype(opt.datadir)>(&opt.datadir), "specify in which directory to search for the data files (default ./data)");
+
+		boost::program_options::variables_map vm;
+		boost::program_options::positional_options_description pos;
+
+		boost::program_options::options_description options("Allowed options");
+		options.add(o_general);
+
+		try
+		{
+			boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(options).positional(pos).run(), vm);
+		} catch(boost::program_options::unknown_option &e)
+		{
+			std::cerr << "Unknown option --" << e.get_option_name() << ", see --help" << std::endl;
+			return -1;
+		}
+
+		try
+		{
+			boost::program_options::notify(vm);
+		} catch(const boost::program_options::required_option &e)
+		{
+			std::cerr << "You forgot this: " << e.what() << std::endl;
+			return 1;
+		}
+
+		if(vm.count("help"))
+		{
+			std::cout
+				<< "Parsing application for Radboud University evaluations. [https://github.com/Wassasin/vakkenranking]" << std::endl
+				<< "Usage: ./vakkenranking [options]" << std::endl
+				<< std::endl
+				<< o_general;
+
+			return 1;
+		}
+
+		if(outputstr == "html" || outputstr == "")
+			opt.output = output_e::HTML;
+		else if(outputstr == "csv")
+			opt.output = output_e::CSV;
+		else
+		{
+			std::cerr << "Unrecognized output type \"" << outputstr << "\"" << std::endl;
+			return 1;
+		}
+
+		return 0;
+	}
+
+	static int act(const options& opt)
+	{
+		auto old_data = parse_data(opt.datadir+"/old", complete_parser::parse);
+		auto new_data = parse_data(opt.datadir+"/new", complete_parser::parse);
+
+		new_data.erase(std::remove_if(
+			new_data.begin(),
+			new_data.end(),
+			[&](const evaluation& x) -> bool
+			{
+				return x.course_grade.count() == 0;
+			}
+		), new_data.end());
+
+		std::sort(
+			new_data.begin(),
+			new_data.end(),
+			[&](const evaluation& a, const evaluation& b) -> bool
+			{
+				return(a.course_grade.avg() > b.course_grade.avg());
+			}
+		);
+
+		if(opt.output == output_e::HTML)
+			printer::print(old_data, new_data);
+		else if(opt.output == output_e::CSV)
+			printer::print_csv(old_data, new_data);
+		else
+			throw std::logic_error("Unimplemented output type");
+
+		return 0;
+	}
+
+public:
+	static int run(int argc, char** argv)
+	{
+		options opt;
+		int result = interpret(opt, argc, argv);
+		if(result != 0)
+			return result;
+		return act(opt);
+	}
+};
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,6 @@
-#include <iostream>
-#include <algorithm>
-#include <sstream>
-#include <map>
-#include <functional>
-
-#include <boost/filesystem.hpp>
-#include <boost/lexical_cast.hpp>
-#include <boost/regex.hpp>
-#include <boost/optional.hpp>
-
-#include "csv_parser.hpp"
-#include "summary_parser.hpp"
-#include "complete_parser.hpp"
-
-#include "printer.hpp"
-
-std::vector<vakkenranking::evaluation> parse_data(const std::string& str, std::function<vakkenranking::evaluation(const boost::filesystem::path&)> parse_f)
-{
-	using namespace vakkenranking;
-	using namespace boost::filesystem;
-
-	std::vector<evaluation> result;
-	
-	path p(str);
-	
-	const directory_iterator it_end;
-	for(directory_iterator it(p); it != it_end; ++it)
-		result.emplace_back(parse_f(it->path()));
-		
-	return result;
-}
+#include "cli.hpp"
 
 int main(int argc, char** argv)
 {
-	using namespace vakkenranking;
-	
-	auto old_data = parse_data("../data/old", complete_parser::parse);
-	auto new_data = parse_data("../data/new", complete_parser::parse);
-	
-	new_data.erase(std::remove_if(
-		new_data.begin(),
-		new_data.end(),
-		[&](const vakkenranking::evaluation& x) -> bool
-		{
-			return x.course_grade.count() == 0;
-		}
-	), new_data.end());
-	
-	std::sort(
-		new_data.begin(),
-		new_data.end(),
-		[&](const vakkenranking::evaluation& a, const vakkenranking::evaluation& b) -> bool
-		{
-			return(a.course_grade.avg() > b.course_grade.avg());
-		}
-	);
-
-	//printer::print(old_data, new_data);
-	printer::print_csv(old_data, new_data);
-	
-	return 0;
+	return vakkenranking::cli::run(argc, argv);
 }
